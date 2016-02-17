b551cccaed70bd78ea9891f03e41706fe13cee85
A_Lippok <alippok@hotmail.co.uk>
2016-02-17 16:30:59 +0000

    Completes sum array: Scott and Adam

--- my_functions.rb
+++ my_functions.rb
@@ -1,5 +1,19 @@
-def add_array_length(array_1, array_2)
-  array_1_length = array_1.length
-  array_2_length = array_2.length
-  return array_1_length + array_2_length
+# def add_array_length(array_1, array_2)
+#   array_1_length = array_1.length
+#   array_2_length = array_2.length
+#   return array_1_length + array_2_length
+# end
+
+def sum_of_array(array)
+  counter = 0#acts like a bank. Each time I arrives at an item, stores it in the counter and moves on
+  for i in array
+    counter += i#need to += so counter stores/holds onto if for the next time it loops through
+  end 
+  return counter
+end
+
+
+def search_array(array)
+  
+
 end
--- my_functions_spec.rb
+++ my_functions_spec.rb
@@ -1,15 +1,23 @@
 require("minitest/autorun")
-require_relative( 'my_functions.rb')
+require_relative("my_functions.rb")
 
 class TestMyFunctions  < MiniTest::Test
 
-def test_add_array_length()
-  result= add_array_length([ 1.23, 6.98, 8.43, 2.45 ], [ 4.23, 1.12, 0.52, 8.67 ])#only invoking the 
-  #method here with 2 arguments. Now to go and design the funtion which will do things with 2 param.
-   assert_equal(8, result)
-end 
+# def test_add_array_length()
+#   result= add_array_length([ 1.23, 6.98, 8.43, 2.45 ], [ 4.23, 1.12, 0.52, 8.67 ])#only invoking the 
+#   #method here with 2 arguments. Now to go and design the funtion which will do things with 2 param.
+#    assert_equal(8, result)
+# end 
 
+def test_sum_of_array()
+ result = sum_of_array([1,2,3,4,5])
+ assert_equal(15, result)
+end
 
+def test_search_array()
+  result = search_array([ 'Hufflepuff', 'Slytherin', 'Gryffindor', 'Ravenclaw' ])
+  assert_equal(true, result)
+end
 
 
 
