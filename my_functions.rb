632d6cdf37a4d2d57929ecd5b866a74d083de5b5
A_Lippok <alippok@hotmail.co.uk>
2016-02-17 19:49:12 +0000

    Adds practise array

--- my_functions.rb
+++ my_functions.rb
@@ -4,29 +4,37 @@
 #   return array_1_length + array_2_length
 # end
 
-def sum_of_array(array)
-  counter = 0#acts like a bank. Each time I arrives at an item, stores it in the counter and moves on
+# def sum_of_array(array)
+#   counter = 0#acts like a bank. Each time I arrives at an item, stores it in the counter and moves on
+#   for i in array
+#     counter += i#need to += so counter stores/holds onto if for the next time it loops through
+#   end 
+#   return counter
+# end
+
+def sum_of_array2(array)
+  counter = 0
   for i in array
-    counter += i#need to += so counter stores/holds onto if for the next time it loops through
-  end 
+    counter += i
+  end
   return counter
 end
 
 
-def search_array(array, item)
-# selection = array.select {|a| a == "Ravenclaw"}
-# select = selection.pop
+# def search_array(array, item)
+# # selection = array.select {|a| a == "Ravenclaw"}
+# # select = selection.pop
 
-# if select == "Ravenclaw"
-# return true
-# else 
-#   return false
+# # if select == "Ravenclaw"
+# # return true
+# # else 
+# #   return false
+# # end
+
+#   for i in array
+#     if i == item
+#       return true
+#     end
+#   end
+# return false
 # end
-
-  for i in array
-    if i == item
-      return true
-    end
-  end
-return false
-end
